2b7212a7d6bb740f4cfe6ce96bd61d60807201d9
David Doherty <david.doherty@engineering.digital.dwp.gov.uk>
2021-05-07 14:40:44 +0100

    creation of index page

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Disability Living Allowance (DLA) for children',
 
   // Default port that prototype runs on
   port: '3000',
